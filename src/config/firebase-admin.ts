f85d43948d676658746d219d346f62d06a82933a
7hokerz <hamahama0925@gmail.com>
2026-01-05 23:40:40 +0900

    feat: 에뮬레이터 설정 추가
    - 에뮬레이터는 docker 컨테이너에서 실행

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -2,7 +2,14 @@ import { getStorage } from "firebase-admin/storage";
 import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 
-export type GCSFile = ReturnType<ReturnType<typeof adminStorage.bucket>["file"]>;
+const isDevelopment = process.env.NODE_ENV === 'development';
+const useEmulator = process.env.USE_FIREBASE_EMULATOR === 'true';
+
+// 에뮬레이터 연결
+if (isDevelopment && useEmulator) {
+  // process.env.STORAGE_EMULATOR_HOST = '127.0.0.1:9199';
+  process.env.FIRESTORE_EMULATOR_HOST = '127.0.0.1:8180';
+}
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
@@ -16,3 +23,5 @@ const adminApp = !getApps().length
 export const adminFirestore = getFirestore(adminApp, "quizgen-db");
 export const adminStorage = getStorage(adminApp);
 export const bucket = adminStorage.bucket('quiz-whiz-hqbig');
+
+export type GCSFile = ReturnType<ReturnType<typeof adminStorage.bucket>["file"]>;
